a22cf1753baf51f764b00a64373f7400803f3f7d
Saswath Singh <saswath@itoneclick.com>
2023-09-01 14:38:04 +0530

    my-date-format package created

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# my-date-format
-An npm package for easy date formatting in JavaScript. Format dates according to custom patterns with this lightweight and versatile utility. Ideal for simplifying date display in web applications.
+# getDateFromFormat
+
+A small utility function to get a formatted date string from a Date object or a string.
+
+## Installation
+
+npm install getDateFromFormat
+
+## Usage
+
+```javascript
+const getDateFromFormat = require('getDateFromFormat');
+
+const date = new Date();
+const formattedDate = getDateFromFormat(date, 'yyyy-mm-dd');
+
+console.log(formattedDate); // 2023-09-01
+
+Parameters
+date: The date to format. Can be a Date object or a string.
+format: The format string. The following tokens are supported:
+yyyy: The year, four digits.
+mmmm: The month, full name.
+mmm: The month, abbreviated name.
+mm: The month, zero-padded number.
+dd: The day, zero-padded number.
+hh: The hour, zero-padded number.
+ii: The minute, zero-padded number.
+ss: The second, zero-padded number.
+day: The day of the week, abbreviated name.
+Returns
+The formatted date string.
+
+Examples
+JavaScript
+getDateFromFormat(new Date(), 'yyyy-mm-dd'); // 2023-09-01
+getDateFromFormat('2023-09-01', 'dd/mm/yyyy'); // 01/09/2023
---
+++ index.js
@@ -0,0 +1,36 @@
+function getDateFromFormat (date, format) {
+    try {
+        // Input validation
+        if (!(date instanceof Date)) {
+            if (typeof date === 'string') {
+                date = new Date(date);
+            } else {
+                throw new Error('Invalid date input. Expected a Date object or a string.');
+            }
+        }
+
+        const replacements = {
+            yyyy: date.getFullYear(),
+            mmmm: date.toLocaleString("default", { month: "long" }),
+            mmm: date.toLocaleString("default", { month: "short" }),
+            mm: ('0' + (date.getMonth() + 1)).slice(-2),
+            dd: ('0' + date.getDate()).slice(-2),
+            hh: ('0' + date.getHours()).slice(-2),
+            ii: ('0' + date.getMinutes()).slice(-2),
+            ss: ('0' + date.getSeconds()).slice(-2),
+            day: date.toLocaleDateString("en-IN", { weekday: "short" })
+        };
+
+        // Using regular expression with a replacement function
+        const result = format.replace(/(yyyy|mmmm|mmm|mm|dd|hh|ii|ss|day)/g, (match, p1) => {
+            return replacements[p1] || match; // If replacement exists, use it; otherwise, keep the original
+        });
+
+        return result;
+    } catch (err) {
+        // Throw a new error or return a default value.
+        throw new Error('Error formatting date: ' + err.message);
+    }
+};
+
+module.exports = getDateFromFormat
